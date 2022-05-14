f303e8496397748149566f9dc72e7dcf68ccac31
Felipe Pinheiro <95388631+fpinheir0@users.noreply.github.com>
2022-05-13 22:45:20 -0300

    Add files via upload

---
+++ MainWindowVM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class MainWindowVM
+    {
+        public ObservableCollection<produtos> ListaProdutos { get; set; }
+
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int QTDE { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+
+        //================================================================================================
+        public RelayCommand Adicionar { get; private set; }
+        public RelayCommand Remover { get; private set; }
+        public RelayCommand Editar { get; private set; }
+        public produtos UsuarioSelecionado { get; set; } 
+        
+        public MainWindowVM()
+        {
+            ListaProdutos = new ObservableCollection<produtos>()
+            {
+                new produtos()
+                {
+                   Codigo = "T-01",
+                   Descricao = "Trator Pequeno",
+                   QTDE = 10,
+                   Marca = "john Deere",
+                   Modelo = "AAGC101"
+                }
+            };
+            IniciaComandos();
+        }
+
+        public void IniciaComandos()
+        {
+            //===========================================================================
+            Adicionar = new RelayCommand( (object _) =>
+            {
+                produtos ProdCadastro = new produtos(); 
+                
+                EditaProduto tela = new EditaProduto();
+                tela.DataContext = ProdCadastro;
+                tela.ShowDialog();
+
+                ListaProdutos.Add(ProdCadastro);
+
+            }
+            );
+            //===========================================================================
+            Editar = new RelayCommand((object _) =>
+            {
+                EditaProduto tela = new EditaProduto();
+                tela.DataContext = UsuarioSelecionado;
+                tela.ShowDialog();
+            }
+            );
+            //===========================================================================
+            Remover = new RelayCommand((object _) =>
+            {
+                ListaProdutos.Remove(UsuarioSelecionado);
+            });
+            //===========================================================================
+        }
+    }
+
+}
---
+++ produtos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+
+    public class produtos
+    {
+        
+        private string codigo;
+        private string descricao;
+        private int qtde;
+        private string marca;
+        private string modelo;
+        private ObservableCollection<produtos> listaProdutos;
+
+        public produtos()
+        {
+
+        }
+
+        public produtos(ObservableCollection<produtos> listaProdutos)
+        {
+            this.listaProdutos = listaProdutos;
+        }
+
+        public produtos(string codigo, string descricao, int qtde, string marca, string modelo)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.qtde = qtde;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        public int QTDE
+        {
+            get { return qtde; }
+            set { qtde = value; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+    }
+}
